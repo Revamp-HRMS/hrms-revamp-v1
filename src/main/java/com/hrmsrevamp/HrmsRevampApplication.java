52c3815d28cbce328e11bc53d0e8c2364d537e3c
Mustaqim Shaikh <mustaqim@apptware.com>
2024-05-10 15:43:48 +0530

    initial setup done

---
+++ src/main/java/com/hrmsrevamp/HrmsRevampApplication.java
@@ -0,0 +1,13 @@
+package com.hrmsrevamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsRevampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmsRevampApplication.class, args);
+	}
+
+}
